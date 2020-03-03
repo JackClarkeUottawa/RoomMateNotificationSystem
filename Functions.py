3d80bebfbe22dc6d21a768f40ce4f33f5374a574
JackClarkeUottawa <48411623+JackClarkeUottawa@users.noreply.github.com>
2020-03-03 17:14:11 -0500

    Rearranged a lot of stuff, removed personal information

--- Functions.py
+++ Functions.py
@@ -3,17 +3,22 @@ import pickle
 from datetime import date
 import os
 import json
+
 def setup():
     "sets up twilio, must be done before running any twilio function"
+
     from twilio.rest import Client
     twi = readfromfile("twilioinfo.json")
-    """twilioinfo.json should look like this:
+
+    """
+    twilioinfo.json should look like this:
     [
     "account_sidgoes here",
     "auth_token goes here"
     ]
     
     """
+
 
     account_sid = twi[0]
     auth_token = twi[1]
@@ -33,6 +38,7 @@ def send_message(bod,num,client):
         to=num
     )
 
+    print(bod + " was sent to " + num)
 def writeArraytofile(a,pat):
     with open(pat,"w") as txt:
         json.dump(a,txt,ensure_ascii=False, indent=4)
@@ -42,9 +48,9 @@ def readfromfile(pat):
         b = json.load(txt)
     return b
 
-def shift(seq, n=0):
-    a = n % len(seq)
-    return seq[-a:] + seq[:-a]
+def shift(seq):
+    a = 1 % len(seq)
+    return seq[a:] + seq[:a]
 
 
 def rearrange_data(pat):
@@ -53,39 +59,41 @@ def rearrange_data(pat):
     if os.path.exists(pat):
         a = readfromfile(pat)
         b = shift(a)
-        writeArraytofile(b)
+        writeArraytofile(b,"schedule.json")
     else:
         print("File not found, creating file...")
         writeArraytofile(["Kitchen","First floor Bathroom","2nd Floor Bathroom","Garbage 1","Garbage 2"],pat)
         #when we have 6 roommates the roles need to change here and a "free" role needs to be added
+
 def mainthing():
     client = setup()
     users = readfromfile("users.json")
 
+
     if date.today().isoweekday() == 5: # day is firday
-        rearrange_data("schedule.txt")# file needs to be changed for server, maybe
-        a = readfromfile("schedule.txt")
+        rearrange_data("schedule.json")# file needs to be changed for server, maybe
+        a = readfromfile("schedule.json")
         for num in range(len(a)):
-            if a[num] != "Garbage 1" or "Garbage 2" or "Free":
-                msg = "Hi "+users[num]["Name"] +"This week you are cleaning the "+a[num]
+            if a[num] not in ["Garbage 1","Garbage 2"]:
+                msg = "Hi "+users[num]["Name"] +" This week you are cleaning the "+a[num]
                 send_message(msg,users[num]["Number"],client)
 
     elif date.today().isoweekday() == 1: # day is monday
-        a = readfromfile("schedule.txt")
+        a = readfromfile("schedule.json")
 
         for num in range(len(a)):
             if a[num] == "Garbage 1":
                 Gp1 = num
             elif a[num] == "Garbage 2":
                 Gp2 = num
-        msg1 = "Hi "+users[Gp1]["Name"] +"This week you are cleaning the Garbage with "+users[Gp2]["Name"]
-        msg2 = "Hi "+users[Gp2]["Name"] +"This week you are cleaning the Garbage with "+users[Gp1]["Name"]
+        msg1 = "Hi "+users[Gp1]["Name"] +" This week you taking out the trash  "+users[Gp2]["Name"]
+        msg2 = "Hi "+users[Gp2]["Name"] +" you're taking out the trassh "+users[Gp1]["Name"]
         send_message(msg1,users[Gp1]["Number"],client)
         send_message(msg2,users[Gp2]["Number"],client)
 
 
 
-
+mainthing()
 
 """Legend
 0 - Jack
